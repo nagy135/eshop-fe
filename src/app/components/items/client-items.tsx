18aa62d295e1c114a203275720bb5830b9866ae7
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-08-05 16:11:19 +0200

    feat: detail styles

--- src/app/components/items/client-items.tsx
+++ src/app/components/items/client-items.tsx
@@ -55,37 +55,55 @@ export default function ClientItems({ items }: IClientItems) {
           </div>
         ))}
       </div>
-      <Modal open={open && !!modalState.item} onClose={handleClose}>
-        <Modal.Header>
-          <Modal.Title className="text-lg capitalize">
-            {modalState.item?.name}
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Image
-            src={`http://api:8000/media/${modalState.item?.image__image[0]}`}
-            width={300}
-            height={300}
-            className="w-full object-contain rounded h-96 group-hover:scale-105 transition duration-200"
-            alt="Banner"
-          />
-          <Placeholder.Paragraph />
-          <Placeholder.Paragraph />
-          <Placeholder.Paragraph />
-          <Placeholder.Paragraph />
-          <Placeholder.Paragraph />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button
-            onClick={handleClose}
-            appearance="primary"
-            className="bg-red-400"
-            color="red"
-          >
-            Cancel
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      {!!modalState.item ? (
+        <Modal open={open} onClose={handleClose}>
+          <Modal.Header>
+            <Modal.Title className="text-lg capitalize">
+              {modalState.item?.name}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="relative">
+              <Image
+                src={`http://api:8000/media/${modalState.item.image__image[0]}`}
+                width={300}
+                height={300}
+                className="w-full object-contain rounded h-96 group-hover:scale-105 transition duration-200"
+                alt="Banner"
+              />
+              <div className="flex flex-row-reverse">
+                <div className="font-bold bg-black/75 text-white p-1 rounded text-lg">
+                  {modalState.item.price}€
+                </div>
+              </div>
+            </div>
+            <div className="mx-2 mt-9 text-justify">
+              {modalState.item.description}
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <div className="m-2">
+              <Button
+                onClick={handleClose}
+                appearance="primary"
+                className="bg-red-400"
+                color="red"
+              >
+                Cancel
+              </Button>
+
+              <Button
+                onClick={handleClose}
+                appearance="primary"
+                className="bg-green-700"
+                color="green"
+              >
+                Buy
+              </Button>
+            </div>
+          </Modal.Footer>
+        </Modal>
+      ) : null}
     </>
   );
 }
