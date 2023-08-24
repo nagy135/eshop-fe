e1fa7e7bc0cb8d86473d544b8f2cabc51ed063b0
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-08-24 11:55:43 +0200

    feat: syncing global bucket

--- src/app/components/bucket.tsx
+++ src/app/components/bucket.tsx
@@ -5,13 +5,12 @@ import { useBucketContext, useSyncedUser } from "../contexts/providers";
 import { getBucket } from "../queries/get-bucket";
 import UserProvider from "../contexts/user";
 import BucketProvider from "../contexts/bucket";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { Item } from "../types";
+import { useQuery } from "@tanstack/react-query";
 import { QueryKeys } from "../queries/enums";
 
 function Inner() {
   const user = useSyncedUser();
-  // const { items: bucketItems, setItems: setBucketItems } = useBucketContext();
+  const { setItems: setBucketItems } = useBucketContext();
 
   const { data: items } = useQuery({
     queryKey: [QueryKeys.BUCKET],
@@ -19,13 +18,10 @@ function Inner() {
     enabled: !!user,
   });
 
-  // useEffect(() => {
-  //   if (user) {
-  //     getBucket(user.id)
-  //       .then((e) => setItems(e))
-  //       .catch((e) => console.log(e));
-  //   }
-  // }, [user]);
+  useEffect(() => {
+    items && setBucketItems(items);
+  }, [items]);
+
   return (
     <div className="bg-red-50 absolute z-50 p-2 w-10 text-center text-2xl rounded left-2 bottom-2 hover:bg-red-500 hover:text-yellow-400 cursor-pointer">
       <div className={items ? "" : "animate-spin"}>
