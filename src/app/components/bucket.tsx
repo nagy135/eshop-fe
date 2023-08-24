4b7a4cbc2e85f1dd1174ce09a42d98f1e70936f9
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-08-24 12:26:58 +0200

    feat: bucket icon

--- src/app/components/bucket.tsx
+++ src/app/components/bucket.tsx
@@ -7,6 +7,7 @@ import UserProvider from "../contexts/user";
 import BucketProvider from "../contexts/bucket";
 import { useQuery } from "@tanstack/react-query";
 import { QueryKeys } from "../queries/enums";
+import { BsFillBasket2Fill } from "react-icons/bs";
 
 function Inner() {
   const user = useSyncedUser();
@@ -23,9 +24,18 @@ function Inner() {
   }, [items]);
 
   return (
-    <div className="fixed bg-red-50 z-50 p-2 w-10 text-center drop-shadow-xl shadow-black text-2xl rounded left-2 top-2 hover:bg-gray-400 hover:text-white cursor-pointer">
+    <div className="fixed bg-red-50 z-50 p-2 text-center border border-gray-400 drop-shadow-xl shadow-black text-2xl rounded left-2 top-2 hover:bg-gray-400 hover:text-white cursor-pointer">
       <div className={items ? "" : "animate-spin"}>
-        {items ? items.length : "⎈"}
+        {items ? (
+          <div className="flex">
+            <span className="flex-1 flex flex-row items-center mr-2">
+              <BsFillBasket2Fill />
+            </span>
+            <span>{items.length}</span>
+          </div>
+        ) : (
+          "⎈"
+        )}
       </div>
     </div>
   );
